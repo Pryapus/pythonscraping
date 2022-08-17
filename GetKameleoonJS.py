f0d1db56852adc2087df92bfd1dde3823777be87
Pryapuz <pryapuz@gmail.com>
2022-08-17 19:17:48 +0200

    Kameleoon JS Scraper

---
+++ GetKameleoonJS.py
@@ -0,0 +1,28 @@
+import requests
+import re
+
+from bs4 import BeautifulSoup
+
+
+domain = "https://www.nutrimuscle.com"
+html = requests.get(domain)
+
+soup = BeautifulSoup(html.text, "html.parser")
+
+# Find all script tags
+for n in soup.find_all('script'):
+
+    # Check if the src attribute exists, and if it does grab the source URL
+    if 'src' in n.attrs:
+        javascript = n['src']
+
+    # Otherwise assume that the javascript is contained within the tags
+    else:
+        javascript = ''
+
+
+    kameleoonRegex = re.compile(r'[\w].*kameleoon.js')
+    #Everything I tried :D
+    kameleeonScript = kameleoonRegex.findall(javascript)
+    if kameleeonScript:
+        print(kameleeonScript[0])
