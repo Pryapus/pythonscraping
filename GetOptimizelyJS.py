952cde98375819377e26c5870df5b3fe873dbde9
Pryapuz <pryapuz@gmail.com>
2022-08-20 14:35:03 +0200

    Extended support for Optimizely

--- GetOptimizelyJS.py
+++ GetOptimizelyJS.py
@@ -1,27 +1,26 @@
-import requests
-import re
+def getOptimizelyScriptSrc(domain):
+    import requests
+    import re
+    from bs4 import BeautifulSoup
+    html = requests.get(domain)
+    soup = BeautifulSoup(html.text, "html.parser")
+    # Find all script tags
+    for n in soup.find_all('script'):
+        # Check if the src attribute exists, and if it does grab the source URL
+        if 'src' in n.attrs:
+            javascript = n['src']
+        # Otherwise assume that the javascript is contained within the tags
+        else:
+            javascript = ''
+        OptimizelyRegex = re.compile(r'cdn.optimizely.com\/js\/.*.js')
+        addhttps = 'https://'
+        # Everything I tried :D
+        OptimizelyScript = OptimizelyRegex.findall(javascript)
+        if OptimizelyScript:
+            print(addhttps + OptimizelyScript[0])
 
-from bs4 import BeautifulSoup
 
 
-domain = "https://www.homechef.com"
-html = requests.get(domain)
 
-soup = BeautifulSoup(html.text, "html.parser")
 
-# Find all script tags
-for n in soup.find_all('script'):
-
-    # Check if the src attribute exists, and if it does grab the source URL
-    if 'src' in n.attrs:
-        javascript = n['src']
-
-    # Otherwise assume that the javascript is contained within the tags
-    else:
-        javascript = ''
-
-    print(javascript)
-    FilterRegex = re.compile(r'https:\/\/cdn.optimizely.com\/js\/.*')
-    OptimzelyScript = FilterRegex.findall(javascript)
-    if OptimzelyScript:
-        print(OptimzelyScript[0])
+getOptimizelyScriptSrc('https://smiledirectclub.com/')
