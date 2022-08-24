ab5943ff816a9650dc0c711b3ff99d6e893a4cad
Pryapuz <pryapuz@gmail.com>
2022-08-24 12:45:43 +0200

    Push to database for src done

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from modules.GetScriptSrc import *
-
+from database.pushjstodatabase import *
+from csvtolist import *
 
 brandlist = ['https://snocks.com', 'https://buah.de', 'https://bitterliebe.com', 'https://hugoboss.com','https://www.piquelife.com/']
 
+for x in ListofBrands:
+    pushtodatabase("JSLinks", "brand_domain", x, "link_js", getScriptSrc(x))
+    print(x + " successfully executed")
 
-for x in brandlist:
-    getScriptSrc(x)
+
